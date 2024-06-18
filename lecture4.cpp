10ea5d7b6c8c2946a8cf71f7b61b2f9c77461e97
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-18 18:24:51 +0530

    Add files via upload

---
+++ lecture4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int a;
+    int b;
+    a = 10;
+    b = 10;
+
+    cout<< a + b <<endl;
+    float x , y;
+    x = 10.5;
+    y = 10.5;
+     cout << x + y << endl;
+
+    double d , e;
+    d = 10.5;
+    e = 10.5;
+
+    cout << d + e << endl;
+
+
+}
